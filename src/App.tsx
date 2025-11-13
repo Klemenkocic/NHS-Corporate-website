bd72918236db47fa82c5b66a646b9e436ec4a542
Klemenkocic <klemen.kocic@gmail.com>
2025-11-13 13:04:57 +0100

    Post iframe height updates to parent

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 import './i18n';
 import HeroSection from './components/HeroSection';
@@ -13,6 +13,34 @@ import BackToTop from './components/BackToTop';
 import CookieConsent from './components/CookieConsent';
 
 const App: React.FC = () => {
+  useEffect(() => {
+    // Function to send height to parent iframe
+    const sendHeight = () => {
+      const height = document.documentElement.scrollHeight;
+      window.parent.postMessage({ type: 'resize', height }, '*');
+    };
+
+    // Send height on load
+    sendHeight();
+
+    // Send height when window resizes
+    window.addEventListener('resize', sendHeight);
+
+    // Send height when content changes (use MutationObserver)
+    const observer = new MutationObserver(sendHeight);
+    observer.observe(document.body, { 
+      childList: true, 
+      subtree: true, 
+      attributes: true 
+    });
+
+    // Cleanup
+    return () => {
+      window.removeEventListener('resize', sendHeight);
+      observer.disconnect();
+    };
+  }, []);
+
   return (
     <>
       <BlobBackground />
